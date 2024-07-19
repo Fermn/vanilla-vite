0ab82b04b7091733b00f303d4d6aa5b455492efb
Fermn <46830086+Fermn@users.noreply.github.com>
2024-07-19 03:39:36 +0000

    initial setup with vite and normalize.css

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+import 'normalize.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello, Vite!</h1>
+  <p>This is a Vanilla Vite app project</p>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
